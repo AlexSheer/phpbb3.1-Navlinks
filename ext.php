ec714b1c88299c3ad2434be9748fc76d7c38a32a
AlexSheer <alex_sheer@inbox.lv>
2015-02-07 10:59:50 +0200

    Create ext.php

---
+++ ext.php
@@ -0,0 +1,16 @@
+<?php
+/**
+*
+* @package navlinks
+* @copyright (c) 2015 Sheer
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+// This file is needed for phpBB3 to properly register the extension
+
+namespace Sheer\navlinks;
+
+class ext extends \phpbb\extension\base
+{
+}
