8fa0ba52228c7d8ec1bc3fe4ee7ef93a706ad322
AlexSheer <alex_sheer@inbox.lv>
2015-02-16 11:57:47 +0200

    Update main_info.php

--- acp/main_info.php
+++ acp/main_info.php
@@ -7,20 +7,20 @@
 *
 */
 
-namespace Sheer\navlinks\acp;
+namespace sheer\navlinks\acp;
 
 class main_info
 {
 	function module()
 	{
 		return array(
-			'filename'	=> '\Sheer\navlinks\acp\main_module',
+			'filename'	=> '\sheer\navlinks\acp\main_module',
 			'version'	=> '1.0.0',
 			'title' => 'ACP_NAV_LINKS_MANAGE',
 			'modes'		=> array(
 				'settings'	=> array(
 					'title' => 'ACP_NAV_LINKS_MANAGE',
-					'auth' => 'ext_Sheer/navlinks && acl_a_board && acl_a_manage_navlinks',
+					'auth' => 'ext_sheer/navlinks && acl_a_board && acl_a_manage_navlinks',
 					'cat' => array('ACP_NAV_LINKS')
 				),
 			),
